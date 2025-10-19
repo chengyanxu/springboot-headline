69e1311accab71d30e22b779f7b6c2286cf9cf06
chengyanxu <3622972886@qq.com>
2025-10-19 08:47:44 +0800

    Update UserController.java

--- src/main/java/com/xuxulearn/springbootheadline/controller/UserController.java
+++ src/main/java/com/xuxulearn/springbootheadline/controller/UserController.java
@@ -1,38 +1,94 @@
-package com.xuxulearn.springbootheadline.controller;
+package com.xuxulearn.springbootheadline.service.impl;
 
+import com.alibaba.druid.util.StringUtils;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xuxulearn.springbootheadline.pojo.User;
 import com.xuxulearn.springbootheadline.service.UserService;
+import com.xuxulearn.springbootheadline.mapper.UserMapper;
+import com.xuxulearn.springbootheadline.utils.JwtHelper;
+import com.xuxulearn.springbootheadline.utils.MD5Util;
 import com.xuxulearn.springbootheadline.utils.Result;
+import com.xuxulearn.springbootheadline.utils.ResultCodeEnum;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
 
-@RestController
-@RequestMapping("user")
-@CrossOrigin//前端访问要跨域
-public class UserController {
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+* @author 36229
+* @description 针对表【news_user】的数据库操作Service实现
+* @createDate 2025-10-15 20:30:11
+*/
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User>
+    implements UserService{
     @Autowired
-    private UserService userService;
-    @PostMapping("/login")
-    public Result login(@RequestBody User user) {
-        Result result =userService.login(user);
-        return result;
-    }
-    @GetMapping("getUserInfo")
-    public Result getUserInfo(@RequestHeader String token){
-        Result result = userService.getUserInfo(token);
-        return result;
+    private UserMapper userMapper;
+    @Autowired
+    private JwtHelper jwtHelper;
+    @Override
+    public Result login(User user){
+//根据前端传回的账户密码，在数据库中取出对应的用户
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<User>();
+        queryWrapper.eq(User::getUsername,user.getUsername());
+        User loginUser = userMapper.selectOne(queryWrapper);
+        if(loginUser==null){
+            return Result.build(null, ResultCodeEnum.USERNAME_ERROR);
+        }
 
+        if(!StringUtils.isEmpty(user.getUserPwd())
+                && MD5Util.encrypt(user.getUserPwd()).equals(loginUser.getUserPwd())){
+            String token = jwtHelper.createToken(loginUser.getUid());
+            Map data = new HashMap();
+            data.put("token", token);
 
+            return Result.ok(data);//如果成功返回200
+        }
+        return Result.build(null, ResultCodeEnum.PASSWORD_ERROR);
     }
-    @PostMapping("checkUserName")
+
+    @Override
+    public Result getUserInfo(String token){
+        boolean expiration = jwtHelper.isExpiration(token);
+        if(expiration){
+            return Result.build(null, ResultCodeEnum.NOTLOGIN);//登录失效，返回401
+        }
+        int userid = jwtHelper.getUserId(token).intValue();
+        User user = userMapper.selectById(userid);
+        user.setUserPwd( null);
+        Map data = new HashMap();
+        data.put("loginuser", user);
+        return Result.ok(data);
+    }
+    @Override
     public Result checkUserName(String username){
-        Result result = userService.checkUserName(username);
-        return result;
+        LambdaQueryWrapper<User> queryWrapper=new LambdaQueryWrapper<>();//查询构造器
+        queryWrapper.eq(User::getUsername,username);//查询数据库中是已有用户名
+        Long count=userMapper.selectCount(queryWrapper);
+        if (count==0) {
+            return Result.ok(null);
+        }
+        return Result.build(null, ResultCodeEnum.USERNAME_USED);// USERNAME_USED表示用户名已被占用
+        //返回4001
+
     }
-    @PostMapping("regist")
-    public Result register(@RequestBody User user){
-        Result result=userService.register(user);
-        return result;
+    @Override
+    public Result register(User user){
+        LambdaQueryWrapper<User> queryWrapper=new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUsername,user.getUsername());
+        Long count=userMapper.selectCount(queryWrapper);
+        if(count>0){
+            return Result.build(null, ResultCodeEnum.USERNAME_USED);
+        }
+        user.setUserPwd(MD5Util.encrypt(user.getUserPwd()));//对密码加密
+        userMapper.insert(user);
+        return Result.ok(null);
     }
+
 }
+
+
+
+
